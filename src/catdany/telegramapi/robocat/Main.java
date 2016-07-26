cd5aec91f434690b216d17bf6ef5ed7032696de4
Dany <dany2001ru@gmail.com>
2016-07-26 15:23:47 +0600

    unused import

--- src/catdany/telegramapi/robocat/Main.java
+++ src/catdany/telegramapi/robocat/Main.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Date;
 
 import catdany.telegramapi.robocat.logging.Log;
 import catdany.telegramapi.robocat.pamphlets.Pamphlets;
